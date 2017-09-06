781ce2f9f0e24ad773c1be7e671030e1a5e2c97e
Abhi Agarwal <me@abhiagarwal.com>
2017-09-06 13:19:17 -0400

    fixing issue

--- emails.js
+++ emails.js
@@ -218,7 +218,7 @@ function getEmails(data, resouceType) {
                     });
                 }
             }, function(err) {
-                deferred.reject(err));
+                deferred.reject(err);
             });
         }, function(err) {
             deferred.reject(err);
---
+++ providers/nylas.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var Q = require('q');
+
+var Nylas = require('nylas').config({
+    appId: 'e64rcq9qza4fke03nm228pnh4',
+    appSecret: '8r89zhzpirjw8umf1zjzln38i'
+});
+
+var nylasFunctions = exports;
