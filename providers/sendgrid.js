4f171c0a9b8bd1e809aa3df35e250ce208f6a8df
Abhi Agarwal <me@abhiagarwal.com>
2017-09-12 15:50:29 -0400

    sendgrid

--- providers/sendgrid.js
+++ providers/sendgrid.js
@@ -3,7 +3,19 @@
 const sgMail = require('@sendgrid/mail');
 var Q = require('q');
 
-var sendgrid = exports;
+// SQS consumer
+var Consumer = require('sqs-consumer');
+var AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: 'us-east-2',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID_GMAIL,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_GMAIL
+});
+
+var sqs = new AWS.SQS({
+    region: 'us-east-2'
+});
 
 function getSendGridApiKey(userBilling) {
     if (userBilling && userBilling.length > 0 && userBilling[0].data && userBilling[0].data.IsOnTrial) {
@@ -12,7 +24,7 @@ function getSendGridApiKey(userBilling) {
     return process.env.SENDGRID_PROD;
 }
 
-function sendEmail(sentryClient, email, user, userBilling, attachments, emailDelay) {
+function sendEmail(email, user, userBilling, attachments, emailDelay) {
     var deferred = Q.defer();
 
     var sendgridApiKey = getSendGridApiKey(userBilling);
@@ -85,4 +97,45 @@ function sendEmail(sentryClient, email, user, userBilling, attachments, emailDel
     return deferred.promise;
 }
 
-sendgrid.sendEmail = sendEmail;
+var app = Consumer.create({
+    region: 'us-east-2',
+    queueUrl: 'https://sqs.us-east-2.amazonaws.com/859780131339/emails-outlook.fifo',
+    handleMessage: (message, done) => {
+        var emailDetails = JSON.parse(message.Body);
+        console.log(emailDetails.email.key.id);
+        var attachments = emailDetails.attachments || [];
+        sendEmail(emailDetails.email, emailDetails.user, emailDetails.userBilling, attachments, emailDetails.emailDelay).then(function(response) {
+            // What we want to send back to the sendEmails function
+            // so we can send that to updates-service
+            var returnEmailResponse = {
+                method: emailDetails.emailMethod,
+                delivered: true,
+                sendid: response.emailId
+            };
+
+            var sqsParams = {
+                MessageBody: JSON.stringify(returnEmailResponse),
+                QueueUrl: 'https://sqs.us-east-2.amazonaws.com/859780131339/emails-updates.fifo',
+                MessageGroupId: emailDetails.user.key.id.toString(),
+                MessageDeduplicationId: emailDetails.email.key.id.toString()
+            };
+
+            sqs.sendMessage(sqsParams, function(err, data) {
+                if (err) {
+                    console.error(err);
+                }
+                done();
+            });
+        }, function(err) {
+            console.error(err);
+            done();
+        });
+    },
+    sqs: sqs
+});
+
+app.on('error', (err) => {
+    console.error(err.message);
+});
+
+app.start();
